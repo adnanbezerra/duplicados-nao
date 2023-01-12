0d1b3ac21755c2be36dec00ea6e93008c143cb96
seu nome <adnanbezerra@gmail.com>
2023-01-12 15:28:52 -0300

    finish exercise

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String[] arrayUm = { "banana", "jaca", "alemão", "uva", "swiatoslav" };
+        String[] arrayDois = { "nanaba", "jacaré", "olimão", "ave", "swiatoslav" };
+        ArrayList<String> saida = buscarSemelhantes(arrayUm, arrayDois);
+
+        System.out.println("Os elementos repetidos são");
+        for(int i = 0; i < saida.size(); i++) {
+            System.out.println(saida.get(i));
+        }
+    }
+
+    private static ArrayList<String> buscarSemelhantes(String[] arrayUm, String[] arrayDois) {
+        ArrayList<String> saida = new ArrayList<>();
+
+        for (int i = 0; i < arrayUm.length; i++) {
+            for (int j = 0; j < arrayDois.length; j++) {
+                if(arrayUm[i].equals(arrayDois[j])) {
+                    saida.add(arrayUm[i]);
+                }
+            }
+        }
+
+        return saida;
     }
 }
